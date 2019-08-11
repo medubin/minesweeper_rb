5d390ede7fdb8c7cb0d227ec3b4a0df3cbffc321
Matt Dubin <medubin@amazon.com>
2019-08-10 17:56:46 -0700

    Add bomb count and timer to banner

--- graphics.rb
+++ graphics.rb
@@ -1,6 +1,7 @@
 require 'ruby2d'
 class Graphics
     TILE = 20
+    BANNER_HEIGHT = 2
     COLORS = {
         -1 => 'red',
         1 => 'blue',
@@ -12,29 +13,36 @@ class Graphics
         7 => 'black',
         8 => 'black'
     }
-    def initialize(size)
+    def initialize(size, bomb_count)
         last = size * TILE
+        offset = TILE * BANNER_HEIGHT
         @size = size
-        @flags = {}
+        @flags = {}        
         size.times do |coord|
-            pos = coord * TILE
-            Line.new(x1: 0, y1: pos, x2: last, y2: pos, width: 1, color: 'black', z: 1)
-            Line.new(x1: pos, y1: 0, x2: pos, y2: last, width: 1, color: 'black', z: 1)
+            x_pos = coord * TILE
+            y_pos = (coord + BANNER_HEIGHT) * TILE
+            Line.new(x1: 0, y1: y_pos, x2: last, y2: y_pos, width: 1, color: 'black', z: 1)
+            Line.new(x1: x_pos, y1: offset, x2: x_pos, y2: last + offset, width: 1, color: 'black', z: 1)
         end
      
-        Window.set(height:size*TILE , width: size*TILE, background: 'silver')
-        
+        Window.set(height:last + offset, width: last, background: 'silver')
+
+        @bomb_count = Text.new(bomb_count, x: TILE / 2, y: TILE / 2, size: TILE, color: 'red', z: 3)
+        @timer = Text.new(0, x: (size * TILE) - TILE * 2, y: TILE / 2, size: TILE, color: 'red', z: 3)
     end
+
+
+    
 
     def reveal(tiles)
         tiles.each do |tile|
-            Square.new( x: tile[:x] * TILE, y: tile[:y] * TILE, size: TILE, color: 'gray', z: 2)
+            y = (tile[:y] + 2) * TILE
+            Square.new( x: tile[:x] * TILE, y: y, size: TILE, color: 'gray', z: 2)
             if (tile[:val] != 0)
                 text = tile[:val] != -1 ? tile[:val] : 'X'
                 color = COLORS[tile[:val]]
-                Text.new(text, x: tile[:x] * TILE + 4, y: tile[:y] * TILE - 2, size: TILE, color: color, z: 3)
+                Text.new(text, x: tile[:x] * TILE + 4, y: y - 2, size: TILE, color: color, z: 3)
             end
-
         end 
     end
 
@@ -46,8 +54,11 @@ class Graphics
         if @flags[[x, y]]
             @flags[[x,y]].remove
             @flags[[x,y]] = nil
+            @bomb_count.text = @bomb_count.text.to_i + 1
         else
-            @flags[[x,y]] = Text.new('P', x: x * TILE + 4, y: y * TILE - 2, size: TILE, color: 'red', z: 3)
+            offset_y = (y + 2)
+            @flags[[x,y]] = Text.new('P', x: x * TILE + 4, y: offset_y * TILE - 2, size: TILE, color: 'red', z: 3)
+            @bomb_count.text = @bomb_count.text.to_i - 1
         end
     end
 
@@ -58,4 +69,13 @@ class Graphics
     def win 
         Text.new('You Win', x:@size  , y: (@size * TILE / 2) - @size * 4, size: @size* 4, color: 'red', z: 4)
     end
+
+    def update_time(sec)
+        time = sec.to_s
+        time = '999' if sec > 999
+        time = '0' + time if time.length <= 1
+        time = '0' + time if time.length <= 2 
+        @timer.text = time
+    end
+
 end
--- minesweeper.rb
+++ minesweeper.rb
@@ -1,17 +1,19 @@
 require_relative './board'
 require_relative './graphics'
 require 'ruby2d'
+require 'date'
 
 class MineSweeper
-    def initialize
+    def initialize(size, bomb_count)
         @playing = true
-        size = 35
-        @board = Board.new(size, 90)
-        @graphics = Graphics.new(size)
+        @start_time = DateTime.now
+        @board = Board.new(size, bomb_count)
+        @graphics = Graphics.new(size, bomb_count)
         Window.on :mouse_down do |event|
             return if !@playing
-            x_tile =  event.x / Graphics::TILE
-            y_tile =  event.y / Graphics::TILE
+            x_tile = event.x / Graphics::TILE
+            y_tile = (event.y / Graphics::TILE) - Graphics::BANNER_HEIGHT
+            next if y_tile < 0
             case event.button
             when :left
                 tiles =  @board.on_click(x_tile, y_tile)
@@ -19,10 +21,17 @@ class MineSweeper
                 check_loss(tiles)
                 check_win()
             when :right
-                @board.toggle_flag(x_tile, y_tile)
+                @flagged = @board.toggle_flag(x_tile, y_tile) 
                 @graphics.toggle_flag(x_tile, y_tile)
             end
         end
+
+        Window.update do
+            now = DateTime.now
+            interval = ((now - @start_time) * 24 * 60 * 60).to_i
+            @graphics.update_time(interval)
+        end
+        
         @graphics.show
     end
 
@@ -43,5 +52,5 @@ class MineSweeper
     end
 end
 
-MineSweeper.new
+MineSweeper.new 35, 90
 
